4458d282337df0e53ad7ec19b4921ecb86a5f7e2
HajarSafiaeddine <lhajar489@gmail.com>
2022-07-05 16:35:42 +0100

    finale

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,32 +1,40 @@
-
-import React, { useEffect, useState } from 'react';
 import axios from "axios";
-import Header from '../components/Header';
-
+import React, { useEffect , useState } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
 
 const UserList = () => {
-    const [listData, setListData] = useState([]);
-    useEffect(() => {
-        let moviesId = window.localStorage.movies
-        ? window.localStorage.movies.split(",")
-        : [];
-        for (let i = 0; i < moviesId.length; i++) { axios.get(  `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=78323d6388bafee535fce5fb030e2dad&language=fr-FR`)
-        .then((res) => setListData((listData) = [...listData, res.data] ));
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d&language=fr-FR`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
     }
-      }, []);
-    return (
-        <div className='user-list-page'>
-            <Header />
-            <h2>coup de coeur<span>❤️</span></h2>
-            <div className="result">
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Coup de coeur <span>💖</span>
+      </h2>
+      <div className="result">
         {listData.length > 0 ? (
-          listData.map((movie) => <Card movie={props} key={movie.id} />)
+          listData.map((movie) => <Card movie={movie} key={movie.id} />)
         ) : (
           <h2>Aucun coup de coeur pour le moment</h2>
         )}
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default UserList;
