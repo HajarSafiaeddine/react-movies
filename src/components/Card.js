4a4cf3ee980427840bd00f350933480eda2e6311
HajarSafiaeddine <lhajar489@gmail.com>
2022-07-05 12:15:57 +0100

    adding component card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,93 @@
 import React from 'react';
 
-const Card = ({ movie }) => {
+const Card = (props) => {
+    const dateFormater = (date) =>{
+      let[yy,mm,dd] = date.split("-");
+      return [dd,mm,yy].join("/");
+    }
+    const genreFinder = () => {
+      let genreArray = [];
+      for (let i = 0; i < props.genre_ids.length; i++) {
+        switch (props.genre_ids[i]) {
+          case 28:
+            genreArray.push(`Action`);
+            break;
+          case 12:
+            genreArray.push(`Aventure`);
+            break;
+          case 16:
+            genreArray.push(`Animation`);
+            break;
+          case 35:
+            genreArray.push(`Comédie`);
+            break;
+          case 80:
+            genreArray.push(`Policier`);
+            break;
+          case 99:
+            genreArray.push(`Documentaire`);
+            break;
+          case 18:
+            genreArray.push(`Drame`);
+            break;
+          case 10751:
+            genreArray.push(`Famille`);
+            break;
+          case 14:
+            genreArray.push(`Fantasy`);
+            break;
+          case 36:
+            genreArray.push(`Histoire`);
+            break;
+          case 27:
+            genreArray.push(`Horreur`);
+            break;
+          case 10402:
+            genreArray.push(`Musique`);
+            break;
+          case 9648:
+            genreArray.push(`Mystère`);
+            break;
+          case 10749:
+            genreArray.push(`Romance`);
+            break;
+          case 878:
+            genreArray.push(`Science-fiction`);
+            break;
+          case 10770:
+            genreArray.push(`Téléfilm`);
+            break;
+          case 53:
+            genreArray.push(`Thriller`);
+            break;
+          case 10752:
+            genreArray.push(`Guerre`);
+            break;
+          case 37:
+            genreArray.push(`Western`);
+            break;
+          default:
+            break;
+        }
+      }
+      return genreArray.map((genre) => <li key={genre}>{genre}</li>);
+    };
     return (
       <div className="card">
-        {/* <img src=''/> */}
-        <h2>{movie.title}</h2>
+         <img
+        src={
+          props.poster_path
+            ? "https://image.tmdb.org/t/p/w500" + props.poster_path
+            : "./img/poster.jpg"
+        }
+        alt="affiche film"
+      />
+        <h2>{props.title}</h2>
+        {props.release_date ? <h5> sorti le : {dateFormater(props.release_date)} </h5> : ""}
+        <h4><span>⭐</span> {props.vote_average}/10 </h4>
+        <ul>{genreFinder()}</ul>
+        {props.overview ? <h3>synopsis</h3> : <p>Ce film ne contient pas de synopsis</p>}<p> {props.overview} </p>
+        <div className="btn">Ajouter aux coups de coeur</div>
       </div>
     );
 };
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -3,25 +3,35 @@ import axios from "axios";
 import Card from './Card';
 const Formulaire = () => {
     const [moviesData, setMoviesData] = useState([]);
+    const [search, setSearch] = useState("code");
+    const [sortGoodBad, setSortGoodBad] = useState(null);
     useEffect(() => {
         axios
           .get(
-            `https://api.themoviedb.org/3/search/movie?api_key=78323d6388bafee535fce5fb030e2dad&query=star&language=fr-FR`
-          ).then((res) => setMoviesData(res.data.results))
-        },[] );
+            `https://api.themoviedb.org/3/search/movie?api_key=78323d6388bafee535fce5fb030e2dad&query=${search}&language=fr-FR`
+          ).then((res) => setMoviesData(res.data.results)) 
+        },[search] );
+    
+        
     return (
       <div className="form-component">
         <div className="form-container">
             <form>
-                <input type="text"  placeholder="Entrer le titre d'un film" id="Search-input"/>
+                <input type="text"  placeholder="Entrer le titre d'un film" id="Search-input"onChange = {(e)=> setSearch(e.target.value)}  />
                 <input type="submit" value="Rechercher" />
             </form>
             <div className="btn-sort-container">
-                <div className="btn-sort" id="goodToBad">Top<span>&#9755;</span></div>
-                <div className="btn-sort" id="badToGood">Bad<span>&#9754;</span></div>
+                <div className="btn-sort" id="goodToBad" onClick={() => setSortGoodBad("goodToBad")}>Top<span>&#9755;</span></div>
+                <div className="btn-sort" id="badToGood" onClick={() => setSortGoodBad("badToGood")}>Bad<span>&#9754;</span></div>
             </div>
         </div>
-        <div className="result">{moviesData.slice(0,12).map((movie) => <Card key={movie.id} movie={movie} />)}</div>
+        <div className="result">{moviesData.sort((a, b) => {
+            if (sortGoodBad === "goodToBad") {
+              return b.vote_average - a.vote_average;
+            } else if (sortGoodBad === "badToGood") {
+              return a.vote_average - b.vote_average;
+            }
+          }).map((movie) => <Card key={movie.id} {...movie} />)}</div>
       </div>
       
     );
